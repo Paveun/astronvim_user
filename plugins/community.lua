7480e63c1d4d8f93b6a60b9b64fbb130c410ac6b
Paveun <a.tresierrajansen@gmail.com>
2024-01-26 14:15:41 -0500

    Add community packs
    add json, docker, json, and markdown packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,4 +7,8 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
 }
